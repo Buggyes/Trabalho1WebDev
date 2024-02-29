8e249fb6675daff484e14d8cfb28470ee5dae7be
buggyes <matheusthja1@gmail.com>
2024-02-29 10:50:04 -0300

    aaa

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 var courses = [];
 
+document.getElementById("lightTheme").addEventListener("click",switchLightTheme);
+document.getElementById("darkTheme").addEventListener("click",switchDarkTheme);
+
 function addCourse() {
     console.log('Enviado');
+}
+
+function switchLightTheme(){
+    document.getElementById('site').className = 'lightTheme';
+}
+
+function switchDarkTheme(){
+    document.getElementById('site').className = 'darkTheme';
 }
