a930ae2225a2ce038aff42054a77516029f6d313
João Vinícius Rosá <rosajv.blu@gmail.com>
2024-04-03 22:26:52 -0300

    Update de login

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 function validateLogin() {
-<<<<<<< Updated upstream
-    // Perform login validation here
-    // If login is successful, redirect to index.html
     window.location.replace("index.html");
   }
-  
-  // Add event listener to prevent form submission and call validateLogin function instead
   const form = document.querySelector('.form_container');
   form.addEventListener('submit', function(event) {
     event.preventDefault();
     validateLogin();
-  });
-=======
-
     window.location.href = "index.html";
   }
->>>>>>> Stashed changes
+);
