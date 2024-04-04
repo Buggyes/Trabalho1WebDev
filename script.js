f27da487706ad727db14e09c7a106f7d67b91ca3
Matheus <matheusthja1@gmail.com>
2024-04-04 09:37:36 -0300

    resolvi bug no login

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("submitButton").addEventListener("click",validateLogin);
+})
+
 function validateLogin() {
-    window.location.replace("index.html");
-  }
-  const form = document.querySelector('.form_container');
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    validateLogin();
-    window.location.href = "index.html";
-  }
-);
+  window.location.href = "index.html";
+};
